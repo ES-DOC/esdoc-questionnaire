9482c85a6d8b71ac5468863d694c25c3b34a00d7
allynt <allyn.treshansky@noaa.gov>
2012-07-31 11:26:02 -0600

    removed superfluous print statements

--- django_cim_forms/models.py
+++ django_cim_forms/models.py
@@ -349,8 +349,6 @@ class MetadataProperty(MetadataModel):
         if (filter.upper()!="ALL"):
             msg = "Error: custom filters are not yet supported for retrieving initial properties"
             raise MetadataError(msg)
-
-        print "calling getProperties"
         
         # finally, return a queryset assuming I want ALL of the newly created properties...
         return cls.objects.filter(pk__in=newProperties)
