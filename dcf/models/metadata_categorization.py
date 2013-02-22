3ef7f912961b8920f5993137ef19400462737fe9
allynt <allyn.treshansky@noaa.gov>
2013-02-21 18:06:18 -0700

    moved schema files to static directory

--- dcf/models/metadata_categorization.py
+++ dcf/models/metadata_categorization.py
@@ -30,7 +30,7 @@ from dcf.utils import *
 
 _UPLOAD_DIR  = "categorizations"
 _UPLOAD_PATH = os.path.join(APP_LABEL,_UPLOAD_DIR)    # this is a relative path (will be concatenated w/ MEDIA_ROOT by FileFIeld)
-_SCHEMA_PATH = os.path.join(settings.MEDIA_ROOT,APP_LABEL,_UPLOAD_DIR,"categorization.xsd") # this is an absolute path
+_SCHEMA_PATH = os.path.join(settings.STATIC_ROOT,APP_LABEL,"xml/categorization.xsd") # this is an absolute path
 
 def validate_categorization_file_extension(value):
     valid_extensions = ["xml"]
