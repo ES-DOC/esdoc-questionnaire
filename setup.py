d8e34371d4055332ecef104cf6398856e198a4bd
allynt <allyn.treshansky@noaa.gov>
2013-10-17 00:28:41 -0600

    test deployment of v0.9.9.6

--- setup.py
+++ setup.py
@@ -1,26 +1,26 @@
-import distribute_setup
-distribute_setup.use_setuptools()
+#!/usr/bin/env python
+
+#import distribute_setup
+#distribute_setup.use_setuptools()
 
 from setuptools import setup, find_packages
 
 setup(
     name='django-cim-forms',
-    version='0.9.9.5',
+    version='0.9.9.6',
     author='Allyn Treshansky',
     author_email='allyn.treshansky@noaa.gov',
     packages=find_packages(),
     url='https://github.com/ES-DOC',
     license='NCSA Open Source License, see LICENSE.txt',
-    description='CIM Editor',
+    description='CIM Questionnaire',
     long_description=open('README.txt').read(),
     zip_safe=False,
     include_package_data=True,
     install_requires=[
-        "Django == 1.4",
+        "Django",
         "distribute",
-        "south == 0.7.3", # required to install on hydra
+        "south", 
         "lxml",
     ],
 )
-
-
