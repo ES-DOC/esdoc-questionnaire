b030f0997e542e8933122b7375c5dc320de808b9
allynt <allyn.treshansky@noaa.gov>
2013-10-17 01:54:35 -0600

    commented out certain urls for v0.9.9.6

--- CIM_Questionnaire/urls.py
+++ CIM_Questionnaire/urls.py
@@ -9,20 +9,20 @@ urlpatterns = patterns('',
     url(r'^admin/',     include(admin.site.urls)),
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
-    # project-specific stuff...
-
-    # project index
-    url(r'^$',         'questionnaire.views.index'),
-    url(r'index/$',    'questionnaire.views.index',    name='questionnaire_index'),
-
-    # project error
-    url(r'^error/$',   'questionnaire.views.error',    name='questionnaire_error'),
-
-    # project help
-    url(r'^help/$',    'questionnaire.views.help',     name='questionnaire_help'),
-
-    # project authentication
-    url(r'^login/$',   'questionnaire.views.login',    name='questionnaire_login'),
+#    # project-specific stuff...
+#
+#    # project index
+#    url(r'^$',         'questionnaire.views.index'),
+#    url(r'index/$',    'questionnaire.views.index',    name='questionnaire_index'),
+#
+#    # project error
+#    url(r'^error/$',   'questionnaire.views.error',    name='questionnaire_error'),
+#
+#    # project help
+#    url(r'^help/$',    'questionnaire.views.help',     name='questionnaire_help'),
+#
+#    # project authentication
+#    url(r'^login/$',   'questionnaire.views.login',    name='questionnaire_login'),
 
     # application-specific stuff...
 
