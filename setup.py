8dd5d4224afd9e7dbffcb05286b9d736c580f23f
allynt <allyn.treshansky@noaa.gov>
2013-12-09 11:30:00 -0700

    added top-level files

--- setup.py
+++ setup.py
@@ -1,26 +1,26 @@
-import distribute_setup
-distribute_setup.use_setuptools()
+#!/usr/bin/env python
+
+#import distribute_setup
+#distribute_setup.use_setuptools()
 
 from setuptools import setup, find_packages
 
 setup(
     name='django-cim-forms',
-    version='0.9.7.2',
+    version='0.10.0.2',  # major.minor.patch (good idea to append deployment id in production)
     author='Allyn Treshansky',
     author_email='allyn.treshansky@noaa.gov',
     packages=find_packages(),
     url='https://github.com/ES-DOC',
     license='NCSA Open Source License, see LICENSE.txt',
-    description='CIM Metadata Entry Form',
-    long_description=open('README.txt').read(),
+    description='CIM Questionnaire',
+    long_description=open('README.md').read(),
     zip_safe=False,
     include_package_data=True,
     install_requires=[
-        "Django == 1.4",
+        "Django",
         "distribute",
-        "south == 0.7.3", # required to install on hydra
+        "south", 
         "lxml",
     ],
 )
-
-
