69d79119b55fc8e28c2ef032180e1adc6e641c13
allynt <allyn.treshansky@noaa.gov>
2014-06-05 16:18:18 -0600

    removed superfluous print statements

--- CIM_Questionnaire/dycore/forms.py
+++ CIM_Questionnaire/dycore/forms.py
@@ -4,13 +4,13 @@ from dycore.models import *
 
 #def setup_dycoreforms():
 try:
-    print "setup_dycoreforms"
+#    print "setup_dycoreforms"
     DycoreScientificProperty_form = MetadataFormFactory(DycoreScientificProperty,name="DycoreScientificProperty_form",form=Property_form)
     DycoreScientificProperty_formset = MetadataFormSetFactory(DycoreScientificProperty,DycoreScientificProperty_form,name="DycoreScientificProperty_formset",extra=0)
 
     DycoreModel_form = MetadataFormFactory(DycoreModel,name="DycoreModel_form",subForms={"responsibleParties":"ResponsibleParty_formset","properties":"DycoreScientificProperty_formset","citations":"Citation_formset"})
     DycoreModel_formset = MetadataFormSetFactory(DycoreModel,DycoreModel_form,name="DycoreModel_formset")
-    print "success"
+#    print "success"
 except DatabaseError:
-    print "error"
+ #   print "error"
     pass
