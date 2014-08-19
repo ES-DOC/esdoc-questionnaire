1f6933685fc3c5b8e905fbcdf2f3c07d6798c3e5
allynt <allyn.treshansky@noaa.gov>
2014-08-19 10:43:47 -0600

    rewrote jquery initialization fns to a) work w/ subforms and b) init_on_show

--- CIM_Questionnaire/questionnaire/static/questionnaire/js/questionnaire_edit.js
+++ CIM_Questionnaire/questionnaire/static/questionnaire/js/questionnaire_edit.js
@@ -4,46 +4,144 @@ var PREVIOUSLY_SELECTED_TAB = 0;
 
 $.ui.dynatree.nodedatadefaults["icon"] = false; // Turn off icons by default
 
-function panes(parent) {
+function panes(element) {
     // hide panes by default
     // only show them as the corresponding tree node is selected
-    $(parent).find(".pane").each(function() {
-        $(this).hide();
-    });
+    $(element).hide();
 }
 
 function autocompletes(parent) {
-    $(".autocomplete").each(function(){
-        var suggestions = $(this).attr("suggestions").split("|");
-        $(this).autocomplete({
-            source : suggestions
-        });
+    var suggestions = $(element).attr("suggestions").split("|");
+    $(elment).autocomplete({
+        source : suggestions
     });
 }
 
-function dynamic_accordions(parent) {
-   /* have to do this in two steps b/c the accordion JQuery method cannot handle any content inbetween accordion panes */
-   /* but I need a container for dynamic formsets to be bound to */
-   /* so _after_ multiopenaccordion() is called, I stick a div into each pane and bind the formset() method to that div */
-    $(parent).find(".accordion .accordion_header").each(function() {
-        var prefix = $(this).closest(".accordion").attr("name");
-        var accordion_wrapper = "<div class='accordion_unit' name='" + prefix + "'></div>";
-        $(this).next().andSelf().wrapAll(accordion_wrapper);
+function dynamic_accordion_buttons(element) {
+
+    if ( $(element).hasClass("add")) {
+        $(element).button({
+            icons: { primary : "ui-icon-circle-plus" },
+            text: true
+        }).click(function(event) {
+            var max = $(event.target).prevAll("input[name='max']").val();
+            var accordion = $(event.target).closest("div.add").prev(".accordion");
+            var n_accordion_panes = $(accordion).find(".accordion_unit").length;
+            if (n_accordion_panes == max) { // don't need to explicitly check where max="*" b/c this comparison will still be untrue
+
+                $("#confirm_dialog").html("Unable to add; this would create more than the maxium number of instances.");
+                $("#confirm_dialog").dialog("option", {
+                    title: "add",
+                    dialogClass: "no_close",
+                    height: 200,
+                    width: 400,
+                    buttons: {
+                        ok: function () {
+                            $(this).dialog("close");
+                        }
+                    }
+                }).dialog("open");
+
+            }
+            else {
+
+                var dynamic_formset_add_button = $(event.target).closest("div.add").prev(".accordion").find(".add-row:last");
+                $(dynamic_formset_add_button).click();
+                // there is function bound to the dynamic-formset add event that will fire after that button is pressed
+                // (I don't have to explicitly call add_subform)
+
+            }
+
+        });
+    }
+
+    if ( $(element).hasClass("remove")) {
+        $(element).button({
+            icons: { primary: "ui-icon-circle-minus" },
+            text: true
+        });
+        /* the click event had to move to its own fn ("remove_subform" defined in questionnaire_edit.js) */
+    }
+
+    if ( $(element).hasClass("replace")) {
+        $(element).button({
+            icons: { primary : "ui-icon-refresh" },
+            text: true
+        }).click(function(event) {
+
+            var accordion = $(event.target).closest(".form").find(".accordion:first");
+
+            $("#confirm_dialog").html("This will remove the current instance and replace it with either an existing or new instance.  You will be unable to undo this operation.  Are you sure you want to continue?");
+            $("#confirm_dialog").dialog("option", {
+                title: "replace",
+                dialogClass: "no_close",
+                height: 200,
+                width: 400,
+                buttons: {
+                    yes : function () {
+
+                        var dynamic_formset_remove_button = $(accordion).find(".delete-row:last");
+                        $(dynamic_formset_remove_button).click();
+                        var dynamic_formset_add_button = $(accordion).find(".add-row:last");
+                        $(dynamic_formset_add_button).click();
+
+
+                        $(this).dialog("close")
+
+                    },
+                    no : function() {
+
+                        $(this).dialog("close");
+
+                    }
+                }
+            }).dialog("open");
+
+        });
+
+    }
+};
+
+function dynamic_accordions(element) {
+    /* element = $(parent).find(".accordion .accordion_header) */
+    /* have to do this in two steps b/c the accordion JQuery method cannot handle any content inbetween accordion panes */
+    /* but I need a container for dynamic formsets to be bound to */
+    /* so _after_ multiopenaccordion() is called, I stick a div into each pane and bind the formset() method to that div */
+    var prefix = $(element).closest(".accordion").attr("name");
+
+    $(element).next().andSelf().wrapAll("<div class='accordion_unit' name='" + prefix + "'></div>");
+
+    var accordion_unit = $(element).closest(".accordion_unit");
+
+    $(accordion_unit).formset({
+       prefix : prefix,
+       formCssClass : "dynamic_accordion_" + prefix,
+       added : function(row) {
+           added_subformset_form(row);
+       },
+       removed : function(row) {
+           removed_subformset_form(row);
+       }
+   });
+
+}
+
+function accordion_headers(element) {
+
+    /* updates the accordion headers based on the current value of a scientific property
+     */
+    $(element).find(".atomic_value").each(function () {
+        $(this).trigger("change");
     });
-
-    $(parent).find(".accordion_unit").each(function() {
-        var prefix = $(this).closest(".accordion").attr("name");
-
-        $(this).formset({
-           prefix : prefix,
-           formCssClass : "dynamic_accordion_" + prefix,
-           added : function(row) {
-               added_subformset_form(row);
-           },
-           removed : function(row) {
-               removed_subformset_form(row);
-           }
-       });
+    $(element).find(".ui-multiselect").each(function () {
+        console.log("found it");
+        var source_name = $(this).prev(".multiselect").attr("name");
+        var target_name = source_name.replace("-enumeration_value", "-scientific_property_value")
+        $(this).find(".multiselect_header").change(function (event) {
+            var source_value = $(this).button("option", "label");
+            var target = $("*[name='" + target_name + "']");
+            $(target).val(source_value);
+        })
     });
 }
 
@@ -72,110 +170,100 @@ function nullables(parent) {
 }
 */
 
-function enumerations(parent) {
+function enumerations(element) {
     // this is a single fn for _both_ open & nullable enumerations
-    $(parent).find(".multiselect.open,.multiselect.nullable").each(function() {
 
-        // whenever this widget changes,
-        // 1) check if NONE is selected
-        // and if so, de-select everything else (and hide the other widget)
-        // 2) check if OTHER is selected
-        // and if so, show the "other" widget
+    // whenever this widget changes,
+    // 1) check if NONE is selected
+    // and if so, de-select everything else (and hide the other widget)
+    // 2) check if OTHER is selected
+    // and if so, show the "other" widget
 
-        $(this).change(function() {
-            other = $(this).siblings("input.other:first");
-            widget = $(this).siblings(".ui-multiselect:first").find(".multiselect_content");
+    $(element).change(function() {
+        other = $(this).siblings("input.other:first");
+        widget = $(this).siblings(".ui-multiselect:first").find(".multiselect_content");
 
-            var values = $(this).find("option:selected").map(function() {
-                return $(this).val();
-            }).get();
+        var values = $(this).find("option:selected").map(function() {
+            return $(this).val();
+        }).get();
 
-            if (values.indexOf("_NONE") != -1) {
-               console.log("TODO: HANDLE .nullable ENUMERATIONS")
-               $(widget).find("label").each(function(){
-                  if ($(this).hasClass("selected")) {
-                      var value = $(this).find("input").val();
-                      if (value != "_NONE") {
-                          $(this).click()
-                      }
-                      $(other).hide()
+        if (values.indexOf("_NONE") != -1) {
+           $(widget).find("label").each(function(){
+              if ($(this).hasClass("selected")) {
+                  var value = $(this).find("input").val();
+                  if (value != "_NONE") {
+                      $(this).click()
                   }
-               });
-            }
+                  $(other).hide()
+              }
+           });
+        }
 
-            else if (values.indexOf("_OTHER") != -1) {
-                $(other).show();
-            }
-            else {
-                $(other).hide();
-            }
-            
-        });
+        else if (values.indexOf("_OTHER") != -1) {
+            $(other).show();
+        }
+        else {
+            $(other).hide();
+        }
 
-        // and do these checks upon initialization too
-        $(this).trigger("change");
     });
+
+    // and do these checks upon initialization too
+    $(element).trigger("change");
 }
 
-function treeviews(parent) {
+function treeviews(element) {
 
-    $(parent).find(".treeview").each(function() {
-        treeview = $(this); // to use later on...
-        $(this).dynatree({
-            debugLevel      : 0,
-            checkbox        : true,
-            selectMode      : 3,
-            minExpandLevel  : 1,
-            activeVisible   : true,
-            onActivate      : function(node) {
-                active_pane_id = node.data.key + "_pane";
-                active_pane = $("#"+active_pane_id);
-                $(active_pane).show();
-                $(active_pane).find(".tabs:first").tabs({"active":PREVIOUSLY_SELECTED_TAB});
-            },
-            onDeactivate    : function(node) {
-                inactive_pane_id = node.data.key + "_pane";
-                inactive_pane = $("#"+inactive_pane_id);
-                $(inactive_pane).hide();
-                PREVIOUSLY_SELECTED_TAB = $(inactive_pane).find(".tabs:first").tabs("option","active");
-            },
-            onSelect        : function(flag,node) {
-                selected_nodes = $(treeview).dynatree("getSelectedNodes");
-                $(treeview).find(".dynatree-partsel:not(.dynatree-selected)").each(function() {
-                    var node = $.ui.dynatree.getNode(this);
-                    selected_nodes.push(node);
-                })
+    $(element).dynatree({
+        debugLevel      : 0,
+        checkbox        : true,
+        selectMode      : 3,
+        minExpandLevel  : 1,
+        activeVisible   : true,
+        onActivate      : function(node) {
+            active_pane_id = node.data.key + "_pane";
+            active_pane = $("#"+active_pane_id);
+            $(active_pane).show();
+            $(active_pane).find(".tabs:first").tabs({"active":PREVIOUSLY_SELECTED_TAB});
+        },
+        onDeactivate    : function(node) {
+            inactive_pane_id = node.data.key + "_pane";
+            inactive_pane = $("#"+inactive_pane_id);
+            $(inactive_pane).hide();
+            PREVIOUSLY_SELECTED_TAB = $(inactive_pane).find(".tabs:first").tabs("option","active");
+        },
+        onSelect        : function(flag,node) {
+            selected_nodes = $(element).dynatree("getSelectedNodes");
+            $(element).find(".dynatree-partsel:not(.dynatree-selected)").each(function() {
+                var node = $.ui.dynatree.getNode(this);
+                selected_nodes.push(node);
+            })
 
-                node.tree.visit(function(node) {
-                   pane_id = node.data.key + "_pane" ;
-                   pane = $("#"+pane_id);
-                   if ($.inArray(node,selected_nodes)>-1) {
-                        $(pane).removeClass("ui-state-disabled");
-                        $(pane).find("input[name$='-active']").prop("checked",true)
-                   }
-                   else {
-                       $(pane).addClass("ui-state-disabled");
-                       $(pane).find("input[name$='-active']").prop("checked",false)
-                   }
-                });
-
-
-            }
-        });
-        root = $(this).dynatree("getRoot");
-        root.visit(function(node) {
-            // TODO: DON'T SELECT EVERYTHING BY DEFAULT
-            node.select(true);
-            node.expand(true);
-        });
-        // root is actually a built-in parent of the tree
-        // and not the first item in my list
-        // hence this fn calll
-        var first_child = root.getChildren()[0];
-        first_child.activate(true);
-
+            node.tree.visit(function(node) {
+               pane_id = node.data.key + "_pane" ;
+               pane = $("#"+pane_id);
+               if ($.inArray(node,selected_nodes)>-1) {
+                    $(pane).removeClass("ui-state-disabled");
+                    $(pane).find("input[name$='-active']").prop("checked",true)
+               }
+               else {
+                   $(pane).addClass("ui-state-disabled");
+                   $(pane).find("input[name$='-active']").prop("checked",false)
+               }
+            });
+        }
     });
-    
+    root = $(element).dynatree("getRoot");
+    root.visit(function(node) {
+        // TODO: DON'T SELECT EVERYTHING BY DEFAULT
+        node.select(true);
+        node.expand(true);
+    });
+    // root is actually a built-in parent of the tree, and not the first item in my list
+    // hence this fn call
+    var first_child = root.getChildren()[0];
+    first_child.activate(true);
+
 }
 
 function inherit(item) {
@@ -248,9 +336,11 @@ function add_subform(row) {
         url += "&s=" + property_id;
     }
 
-    /* TODO: DOUBLE-CHECK THAT THIS WILL WORK W/ LESS THAN 2 FORMS */
+
     var old_prefix = $(row).closest(".accordion").attr("name");
-    old_prefix += "-" + (n_forms - 2);
+    /* TODO: DOUBLE-CHECK THAT THIS IS ALWAYS CREATING A NEWFORM W/ ID=0 */
+    /*old_prefix += "-" + (n_forms - 2);*/
+    old_prefix += "-" + "0"
 
     var add_subform_dialog = $("#add_dialog");
 
@@ -270,10 +360,11 @@ function add_subform(row) {
                     var parent = $(add_subform_dialog);
                     // the addition of the 'true' attribute forces initialization,
                     // even if this dialog is opened multiple times
-                    init_widget(buttons,parent,true);
-                    init_widget(fieldsets,parent,true);
-                    init_widget(selects,parent,true);
-                    init_widget(helps,parent,true);
+                    init_widgets(buttons,$(parent).find("input.button"),true);
+                    init_widgets(fieldsets,$(parent).find(".collapsible_fieldset"),true);
+                    init_widgets(helps,$(parent).find(".help_button"),true);
+                    init_widgets(selects,$(parent).find(".multiselect"),true);
+
                 },
 
                 buttons : [
@@ -296,27 +387,44 @@ function add_subform(row) {
 
                                     if (status_code == 200 ) {
 
-                                        /* TODO: CHECK DATATYPE OF DATA? */
                                         var parsed_data = $.parseJSON(data);
-
                                         var new_prefix = parsed_data.prefix
 
-                                        // rename ids and names
+                                        console.log(old_prefix);
+                                        console.log(new_prefix);
+                                        console.log(parsed_data);
+
+                                        /* rename ids and names */
                                         update_field_names(row,old_prefix,new_prefix);
+                                        /* insert data */
                                         populate_form(row,parsed_data);
+
+                                        /* initialize JQuery widgets */
+                                        $(row).ready(function() {
+
+                                            init_widgets_on_show(fieldsets,$(row).find(".collapsible_fieldset"))
+                                            init_widgets_on_show(helps,$(row).find(".help_button"));
+                                            init_widgets_on_show(readonlies,$(row).find(".readonly"));
+                                            init_widgets_on_show(dates,$(row).find(".datetime,.date"));
+                                            init_widgets_on_show(accordions,$(row).find(".accordion").not(".fake"));
+                                            init_widgets_on_show(dynamic_accordions,$(row).find(".accordion .accordion_header"));
+                                            init_widgets_on_show(accordion_buttons,$(row).find(".subform_toolbar button"));
+                                            init_widgets_on_show(dynamic_accordion_buttons,$(row).find("button.add,button.remove,button.replace"));
+                                            init_widgets_on_show(selects,$(row).find(".multiselect"));
+                                            init_widgets_on_show(enumerations,$(row).find(".multiselect.open,.multiselect.nullable"));
+                                            init_widgets_on_show(autocompletes,$(row).find(".autocomplete"));
+                                            init_widgets_on_show(enablers,$(row).find(".enabler"));
+
+                                        });
 
                                         $(add_subform_dialog).dialog("close");
 
-
-                                        alert($(row).attr("class"));
-                                        alert($(row).attr("name"));
-                                        // TODO: INIT WIDGETS IN NEW FORM?
                                     }
                                     else {
 
                                         /*
                                          note - do not use a status code of 400 for form valiation errors
-                                         that will be routed to the "error" event below
+                                         that will be routed to the "error" event above
                                          instead use some valid success code other than 200 (202, for example)
                                         */
 
@@ -344,6 +452,7 @@ function add_subform(row) {
                                         var parent = $(add_subform_dialog);
                                         // the addition of the 'true' attribute forces initialization,
                                         // even if this dialog is opened multiple times
+                                        // TODO
                                         init_widget(buttons,parent,true);
                                         init_widget(fieldsets,parent,true);
                                         init_widget(selects,parent,true);
