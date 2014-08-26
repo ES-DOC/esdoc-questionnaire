d52a7f4d85f09cc56b9fdb8faa716331db60e086
allynt <allyn.treshansky@noaa.gov>
2014-08-26 16:30:34 -0600

    updated migration script to account for existing scientific customizers

--- CIM_Questionnaire/db_migrate_from_0_11_0_1.py
+++ CIM_Questionnaire/db_migrate_from_0_11_0_1.py
@@ -16,6 +16,9 @@ if __name__ == "__main__":
     # hence the try/except block in settings.py for "fixing known django / south / postgres issue")
 
     from django.contrib.contenttypes.models import ContentType
+    from django.template.defaultfilters import slugify
+
+    from CIM_Questionnaire.questionnaire.models import *
 
     ComponentClass = ContentType.objects.get(app_label="questionnaire", model="metadatacomponentproxy").model_class()
     component_qs = ComponentClass.objects.all()
@@ -30,3 +33,29 @@ if __name__ == "__main__":
         if not vocabulary.guid:
             vocabulary.guid = str(uuid4())
             vocabulary.save()
+
+    for vocabulary in MetadataVocabulary.objects.all():
+
+        old_vocabulary_key = slugify(vocabulary.name)
+        new_vocabulary_key = vocabulary.get_key()
+
+        for component in vocabulary.component_proxies.all():
+
+            old_component_key = slugify(component.name)
+            new_component_key = component.get_key()
+
+            scientific_category_customizers = MetadataScientificCategoryCustomizer.objects.filter(vocabulary_key=old_vocabulary_key, component_key=old_component_key)
+            for scientific_category_customizer in scientific_category_customizers:
+                scientific_category_customizer.vocabulary_key = new_vocabulary_key
+                scientific_category_customizer.component_key = new_component_key
+                scientific_category_customizer.model_key = u"%s_%s" % (new_vocabulary_key, new_component_key)
+                scientific_category_customizer.save()
+
+            scientific_property_customizers = MetadataScientificPropertyCustomizer.objects.filter(vocabulary_key=old_vocabulary_key, component_key=old_component_key)
+            for scientific_property_customizer in scientific_property_customizers:
+                scientific_property_customizer.vocabulary_key = new_vocabulary_key
+                scientific_property_customizer.component_key = new_component_key
+                scientific_property_customizer.model_key = u"%s_%s" % (new_vocabulary_key, new_component_key)
+                scientific_property_customizer.save()
+
+                
