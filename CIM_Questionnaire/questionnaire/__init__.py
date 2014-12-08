228bffec6fb9e8b093baedb186f6441b9bb86f79
allynt <allyn.treshansky@noaa.gov>
2014-12-08 13:22:34 -0700

    updated version info

--- CIM_Questionnaire/questionnaire/__init__.py
+++ CIM_Questionnaire/questionnaire/__init__.py
@@ -1,11 +1,28 @@
-__author__= "allyn.treshansky"
-__date__= "$Sep 30, 2013 4:12:23 PM$"
+####################
+#   ES-DOC CIM Questionnaire
+#   Copyright (c) 2014 ES-DOC. All rights reserved.
+#
+#   University of Colorado, Boulder
+#   http://cires.colorado.edu/
+#
+#   This project is distributed according to the terms of the MIT license [http://www.opensource.org/licenses/MIT].
+####################
+
+__author__ = "allyn.treshansky"
+__date__ = "Dec 01, 2014 3:00:00 PM"
+
+"""
+.. module:: __init__
+
+init module for "questionnaire" application.
+deals w/ version info.
+"""
 
 APP_LABEL = "questionnaire"
 
 __version_info__ = {
-    'major': 0.11,
-    'minor': 3,
+    'major': 0.12,
+    'minor': 0,
     'patch': 0,
 }
 
