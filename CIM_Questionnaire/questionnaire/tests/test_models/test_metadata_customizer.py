c3a19f672f473616933ddce15e0c388c6fceeadd
Allyn Treshansky <allyn.treshansky@noaa.gov>
2015-02-03 17:07:22 -0700

    tests for customizer

--- CIM_Questionnaire/questionnaire/tests/test_models/test_metadata_customizer.py
+++ CIM_Questionnaire/questionnaire/tests/test_models/test_metadata_customizer.py
@@ -1,58 +1,39 @@
 ####################
-#   CIM_Questionnaire
-#   Copyright (c) 2013 CoG. All rights reserved.
+#   ES-DOC CIM Questionnaire
+#   Copyright (c) 2014 ES-DOC. All rights reserved.
 #
-#   Developed by: Earth System CoG
 #   University of Colorado, Boulder
 #   http://cires.colorado.edu/
 #
 #   This project is distributed according to the terms of the MIT license [http://www.opensource.org/licenses/MIT].
 ####################
 
-import json
+__author__ = 'ben.koziol'
+__date__ = "Dec 01, 2014 3:00:00 PM"
 
-from django.core.urlresolvers import reverse
-from django.contrib import messages
+"""
+.. module:: test_metadata_customizer
 
-from CIM_Questionnaire.questionnaire.tests.base import TestQuestionnaireBase
+Tests the MetadataCustomizer models
+"""
 
-from CIM_Questionnaire.questionnaire.models.metadata_customizer import MetadataCustomizer, MetadataModelCustomizer
+from CIM_Questionnaire.questionnaire.tests.test_base import TestQuestionnaireBase
+from CIM_Questionnaire.questionnaire.models.metadata_customizer import *
 from CIM_Questionnaire.questionnaire.models.metadata_proxy import MetadataModelProxy
 
-from CIM_Questionnaire.questionnaire.forms.forms_customize import get_data_from_customizer_forms
-
-from CIM_Questionnaire.questionnaire.fields import MetadataFieldTypes
-
-from CIM_Questionnaire.questionnaire.utils import add_parameters_to_url
 
 class TestMetadataCustomizer(TestQuestionnaireBase):
 
-
-    def setUp(self):
-
-        super(TestMetadataCustomizer,self).setUp()
-
-        # setup an additional customizer for testing purposes that uses subforms
-        test_proxy = MetadataModelProxy.objects.get(version=self.version, name__iexact="modelcomponent")
-        test_customizer = self.create_customizer_set_with_subforms(self.project, self.version, test_proxy, properties_with_subforms=["author"])
-
-        self.model_customizer.default = False
-        self.model_customizer.save()
-        test_customizer.default = True
-        test_customizer.save()
-
-        self.test_customizer = test_customizer
-
     def test_get_existing_customizer_set(self):
 
-        test_customizer = self.test_customizer
-        test_vocabularies = test_customizer.project.vocabularies.filter(document_type__iexact=test_customizer.proxy.name.lower())
+        test_customizer = self.downscaling_model_component_customizer_set_with_subforms["model_customizer"]
+        test_vocabularies = self.downscaling_model_component_vocabularies
 
-        (model_customizer,standard_category_customizers, standard_property_customizers, scientific_category_customizers,scientific_property_customizers) = \
+        (model_customizer, standard_category_customizers, standard_property_customizers, scientific_category_customizers, scientific_property_customizers) = \
             MetadataCustomizer.get_existing_customizer_set(test_customizer, test_vocabularies)
 
         # make sure the model_customizer is as expected
-        self.assertEqual(model_customizer,test_customizer)
+        self.assertEqual(model_customizer, test_customizer)
 
         # make sure the standard category customizers are as expected
         self.assertQuerysetEqual(standard_category_customizers, test_customizer.standard_property_category_customizers.all())
@@ -68,7 +49,7 @@ class TestMetadataCustomizer(TestQuestionnaireBase):
                 # make sure the scientific category customizers are as expected
                 test_scientific_category_customizer_qs_from_model = test_customizer.scientific_property_category_customizers.filter(vocabulary_key=vocabulary_key, component_key=component_key)
                 scientific_category_customizer_qs = scientific_category_customizers[vocabulary_key][component_key]
-                self.assertQuerysetEqual(test_scientific_category_customizer_qs_from_model,scientific_category_customizer_qs)
+                self.assertQuerysetEqual(test_scientific_category_customizer_qs_from_model, scientific_category_customizer_qs)
 
                 # make sure the scientific property customizers are as expected
                 test_scientific_property_customizer_qs_from_model = test_customizer.scientific_property_customizers.filter(vocabulary_key=vocabulary_key, component_key=component_key)
@@ -77,10 +58,10 @@ class TestMetadataCustomizer(TestQuestionnaireBase):
 
     def test_rename_customizer_set(self):
 
-        test_customizer = self.test_customizer
+        test_customizer = self.downscaling_model_component_customizer_set_with_subforms["model_customizer"]
 
-        name_1 = "customizer_with_subforms"
-        name_2 = "customizer_with_subforms_new"
+        name_1 = "subforms"
+        name_2 = "new_name"
 
         self.assertEqual(test_customizer.name, name_1)
         for standard_property_customizer in test_customizer.standard_property_customizers.all():
