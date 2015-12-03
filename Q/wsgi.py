8c04c841f0a0e6abbf117b8ed36c91e235c10179
Allyn Treshansky <allyn.treshansky@noaa.gov>
2015-12-03 16:37:12 -0700

    added support for virtualenvironments under Apache

--- Q/wsgi.py
+++ Q/wsgi.py
@@ -13,14 +13,39 @@ WSGI config for Q project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
+It also has some added code to cope w/ virtual environments
+
 For more information on this file, see
 https://docs.djangoproject.com/en/1.8/howto/deployment/wsgi/
 """
 
-import os
+import os, sys, site
+from ConfigParser import SafeConfigParser, NoSectionError
 
-from django.core.wsgi import get_wsgi_application
+rel = lambda *x: os.path.join(os.path.abspath(os.path.dirname(__file__)), *x)
+
+CONF_PATH = os.path.join(os.path.expanduser('~'), '.config', 'esdoc-questionnaire.conf')
+parser = SafeConfigParser()
+parser.read(CONF_PATH)
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Q.settings")
 
+try:
+    # if I am using mod_wsgi w/ a virtualenv
+    # then make sure to activate the environment prior to running the application
+    VIRTUALENV_PATH = parser.get('virtualenv', 'path')
+    site.addsitedir(VIRTUALENV_PATH + '/lib/python2.7/site-packages')
+    activate_this = os.path.expanduser(VIRTUALENV_PATH + '/bin/activate_this.py')
+    execfile(activate_this, dict(__file__=activate_this))
+except NoSectionError:
+    pass
+
+# workspace = os.path.dirname(rel('.'))
+# sys.path.append(workspace)
+sys.path = [
+    rel('..'),
+    rel('.'),
+] + sys.path
+
+from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
