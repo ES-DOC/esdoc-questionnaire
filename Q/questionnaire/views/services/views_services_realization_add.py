d80e67c789e7bed71d3aa1cc5ad6bab0cac3137a
Allyn Treshansky <allyn.treshansky@colorado.edu>
2016-10-11 13:10:20 +0100

    ability to work w/ a mixture of saved & unsaved models in relationship fields, as per #481

--- Q/questionnaire/views/services/views_services_realization_add.py
+++ Q/questionnaire/views/services/views_services_realization_add.py
@@ -14,7 +14,8 @@ from django.http import HttpResponseForbidden, HttpResponseBadRequest, JsonRespo
 from django.template.context import RequestContext
 
 from Q.questionnaire.models.models_proxies import QModelProxy
-from Q.questionnaire.models.models_realizations import QModel, get_new_realizations, get_model_realization_by_fn, get_property_realization_by_fn, serialize_new_realizations
+from Q.questionnaire.models.models_realizations import QModel, get_new_realizations, get_model_realization_by_fn, get_property_realization_by_fn, serialize_new_realizations, recurse_through_realizations, RealizationTypes
+from Q.questionnaire.models.models_realizations import get_realization_path, walk_realization_path
 from Q.questionnaire.views.views_base import get_key_from_request, get_cached_object
 from Q.questionnaire.views.services.views_services_base import validate_request
 from Q.questionnaire.q_fields import allow_unsaved_fk
@@ -23,6 +24,7 @@ from Q.questionnaire.q_utils import QError
 # the purpose of this service view is 2-fold...
 # 1st it creates a new realization and adds it to the appropriate realizations already cached on the server
 # 2nd it creates a JSON serialization of that realization to append to the JSON object already loaded on the client
+
 
 def q_realization_add_relationship_value(request):
     valid_request, msg = validate_request(request)
@@ -68,7 +70,7 @@ def q_realization_add_relationship_value(request):
         # which is a fk relationship; hence this extra bit of code (which only exists so that "model_realizations.py#QRealization.get_parent_model_realization" works)
         new_model_realization.relationship_property = property
 
-    # TODO: IS THERE ANY WAY TO AVOID RE-CACHING THE MODEL AFTER MODIFYING IT?  IT SEEMS UNNECESSARY?
+    # re-cache the changed realizations...
     request.session[cached_realizations_key] = cached_realizations
 
     # and return a serialized version of that model...
--- Q/questionnaire/views/services/views_services_realization_remove.py
+++ Q/questionnaire/views/services/views_services_realization_remove.py
@@ -13,15 +13,17 @@ __author__ = "allyn.treshansky"
 from django.http import HttpResponseForbidden, HttpResponseBadRequest, JsonResponse
 from django.contrib import messages
 
-from Q.questionnaire.models.models_realizations import QModel, get_new_realizations, get_model_realization_by_fn, get_property_realization_by_fn
+from Q.questionnaire.models.models_realizations import get_model_realization_by_fn, get_property_realization_by_fn
 from Q.questionnaire.views.views_base import get_key_from_request, get_cached_object
 from Q.questionnaire.views.services.views_services_base import validate_request
 from Q.questionnaire.q_utils import QError
 
-# this removes the specified lated realization from the cached realization
-# strictly speaking, I don't think this is necessary b/c JSON object on the client will replace that cache upon saving
-# however, this does provide an end point for "q_ng_editor.js#remove_relationship_value_aux",
-# which has the effect of refreshing the display after "q_ng_editor.js#remove_relationship_value" has already removed the appropriate JSON bit
+# this removes the specified realization [model] from the cached realization [property]
+# strictly speaking, this isn't necessary b/c the JSON object on the client will replace that cache upon serialization
+# however, this does provide an end point for "q_ng_editor.js#remove_relationship_value_aux", which has the effect of
+# refreshing the display after "q_ng_editor.js#remove_relationship_value" has already removed the appropriate JSON bit
+# and, anyway, it follows the same logic as "q_realization_add_relationship_value"
+
 
 def q_realization_remove_relationship_value(request):
     valid_request, msg = validate_request(request)
@@ -46,25 +48,19 @@ def q_realization_remove_relationship_value(request):
     if not property:
         raise QError("unable to find property w/ key='{0}'".format(property_key))
 
-    # double-check that removing this model from this property makes sense...
-    assert property.relationship_values(manager="allow_unsaved_relationship_values_manager").count() > target_index
-
     # remove the model...
-    # TODO: IS THERE A MORE EFFICIENT WAY IN DJANGO TO REMOVE AN OBJECT AT A SPECIFIC INDEX FROM A QS
-    for i, target in enumerate(property.relationship_values(manager="allow_unsaved_relationship_values_manager").all()):
-        if i == target_index:
-            property.relationship_values(manager="allow_unsaved_relationship_values_manager").remove_potentially_unsaved(target)
-            try:
-                target.delete()
-            except:
-                print "TODO: DOES THIS ONLY FAIL FOR UNSAVED MODELS?"
-                pass
-            break
+    try:
+        target_to_remove = property.relationship_values(manager="allow_unsaved_relationship_values_manager").all()[target_index]
+        property.relationship_values(manager="allow_unsaved_relationship_values_manager").remove_potentially_unsaved(target_to_remove)
+        if target_to_remove.is_existing():
+            target_to_remove.delete()
+    except IndexError:
+        raise QError("unable to find target of {0} at index {1}".format(property, target_index))
 
-    # TODO: IS THERE ANY WAY TO AVOID RE-CACHING THE MODEL AFTER MODIFYING IT?  IT SEEMS UNNECESSARY?
+    # re-cache the changed realizations...
     request.session[cached_realizations_key] = cached_realizations
 
-    # return a success msg...
+    # and return a success msg...
     # TODO: WHY DOESN'T THIS MSG DISPLAY IMMEDIATELY?
     msg = "Successfully removed object"
     messages.add_message(request, messages.SUCCESS, msg)
