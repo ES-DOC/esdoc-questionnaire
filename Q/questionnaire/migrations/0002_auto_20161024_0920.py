77ebd3083a209df80483e06d5163778ce8aa5a0c
Allyn Treshansky <allyn.treshansky@colorado.edu>
2016-10-24 10:22:59 +0100

    updated migrations

--- Q/questionnaire/migrations/0002_auto_20161024_0920.py
+++ Q/questionnaire/migrations/0002_auto_20161024_0920.py
@@ -8,10 +8,39 @@ import Q.questionnaire.q_utils
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('questionnaire', '0003_auto_20161017_1521'),
+        ('questionnaire', '0001_initial'),
     ]
 
     operations = [
+        migrations.RemoveField(
+            model_name='qmodelthing',
+            name='ontology',
+        ),
+        migrations.RemoveField(
+            model_name='qmodelthing',
+            name='project',
+        ),
+        migrations.RemoveField(
+            model_name='qmodelthing',
+            name='proxy',
+        ),
+        migrations.RemoveField(
+            model_name='qmodelthing',
+            name='relationship_source_property_customization',
+        ),
+        migrations.RemoveField(
+            model_name='qpropertything',
+            name='model_customization',
+        ),
+        migrations.RemoveField(
+            model_name='qpropertything',
+            name='proxy',
+        ),
+        migrations.AddField(
+            model_name='qproject',
+            name='is_legacy',
+            field=models.BooleanField(default=False, help_text=b"A legacy project is one that still uses CIM1 and requests must therefore be routed through the legacy site.  Do not check this unless you really know what you're doing."),
+        ),
         migrations.AlterField(
             model_name='qcategorycustomization',
             name='category_title',
@@ -28,14 +57,14 @@ class Migration(migrations.Migration):
             field=models.CharField(help_text=b'A unique name for this customization.  Spaces or the following characters are not allowed: "\\, /, <, >, %, #, %, {, }, [, ], $, |".', max_length=128, verbose_name=b'Customization Name', validators=[Q.questionnaire.q_utils.ValidateNoBadChars(), Q.questionnaire.q_utils.ValidateNoSpaces(), Q.questionnaire.q_utils.ValidateNoReservedWords(), Q.questionnaire.q_utils.ValidateNoProfanities()]),
         ),
         migrations.AlterField(
-            model_name='qmodelthing',
-            name='name',
-            field=models.CharField(max_length=128, verbose_name=b'Customization Name', validators=[Q.questionnaire.q_utils.ValidateNoBadChars(), Q.questionnaire.q_utils.ValidateNoSpaces(), Q.questionnaire.q_utils.ValidateNoReservedWords(), Q.questionnaire.q_utils.ValidateNoProfanities()]),
-        ),
-        migrations.AlterField(
             model_name='qontology',
             name='name',
             field=models.CharField(max_length=128, validators=[Q.questionnaire.q_utils.ValidateNoSpaces(), Q.questionnaire.q_utils.ValidateNoBadChars()]),
+        ),
+        migrations.AlterField(
+            model_name='qproject',
+            name='is_active',
+            field=models.BooleanField(default=True, help_text=b'A project that is not active cannot be used'),
         ),
         migrations.AlterField(
             model_name='qproject',
@@ -57,14 +86,10 @@ class Migration(migrations.Migration):
             name='property_title',
             field=models.CharField(max_length=128, validators=[Q.questionnaire.q_utils.ValidateNoProfanities()]),
         ),
-        migrations.AlterField(
-            model_name='qpropertything',
-            name='name',
-            field=models.CharField(max_length=128, verbose_name=b'Customization Name', validators=[Q.questionnaire.q_utils.ValidateNoBadChars(), Q.questionnaire.q_utils.ValidateNoSpaces(), Q.questionnaire.q_utils.ValidateNoReservedWords(), Q.questionnaire.q_utils.ValidateNoProfanities()]),
+        migrations.DeleteModel(
+            name='QModelThing',
         ),
-        migrations.AlterField(
-            model_name='qpropertything',
-            name='property_title',
-            field=models.CharField(max_length=128, validators=[Q.questionnaire.q_utils.ValidateNoProfanities()]),
+        migrations.DeleteModel(
+            name='QPropertyThing',
         ),
     ]
